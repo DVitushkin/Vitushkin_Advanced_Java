d0c27b1b5691f7d5d8d4e571a428efaae6585d9a
DVitushkin <d.vitushkin@mk.dunice.net>
2023-12-13 11:32:57 +0300

    Add package with implementation of Student entity
    Added Student class and demonstrate how to work with Students collection
    by Stream API functions
    
    Task#12
    	.a
    	.b
    Card#3

---
+++ src/main/java/org/DVitushkin/streamtasks/studentstask/Student.java
@@ -0,0 +1,41 @@
+package org.DVitushkin.streamtasks.studentstask;
+
+/**
+ * Class describe student entity.
+ */
+
+public class Student {
+    private String name;
+    private int age;
+    private Sex sex;
+
+    public Student(String name, int age, Sex sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+}
